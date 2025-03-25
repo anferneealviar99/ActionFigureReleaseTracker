1f6189b1eb114a8ca2887b9faa58e4ed1fae4bdc
anferneealviar99 <anferneealviar99@hotmail.com>
2025-03-25 11:31:57 +0800

    green rock web scrape

---
+++ src/web_scraper.py
@@ -0,0 +1,61 @@
+import requests
+from bs4 import BeautifulSoup as bs
+
+HEADERS = {"User-Agent": "Mozilla/5.0"}
+
+# Green Rock Comics
+SHOP_URLS = {"green_rock": "https://www.greenrockcomics.shop",}
+
+LINK_URLS = {
+    "green_rock_dc": "https://www.greenrockcomics.shop/collections/dc-multiverse",
+    "green_rock_marvel": "https://www.greenrockcomics.shop/collections/marvel-toys",
+    "green_rock_transformers": "https://www.greenrockcomics.shop/collections/transformers"
+}
+
+SHOP = {
+    "green_rock": "Green Rock Comics",
+    "culture_shock": "Culture Shock Collectables",
+    "popcultcha": "Popcultcha",
+    "zing": "ZING Pop Culture",
+}
+
+def scrape(link_url, shop_url, shop_name):
+    figures = []
+    
+    if shop_name is SHOP["green_rock"]:
+        response = requests.get(link_url, header=HEADERS)
+        soup = bs(response.text, "html.parser")
+        products = soup.find_all("div", class_="product-details")
+    
+        for product in products:
+            name = product.find("div", class_="product-title").text.strip()
+            price = product.find("div", class_='product-price').text.strip()
+            link = shop_url + product.find("a")["href"]
+
+            if link_url is LINK_URLS["green_rock_dc"]:
+                figures.append({'line': 'McFarlane DC',
+                                'name': name,
+                                'price': price,
+                                'link': link})
+                
+            elif link_url is LINK_URLS["green_rock_marvel"]:
+                figures.append({'line': 'Marvel Legends',
+                                'name': name,
+                                'price': price,
+                                'link': link})
+                
+            elif link_url is LINK_URLS["green_rock_transformers"]:
+                figures.append({'line': 'Transformers',
+                                'name': name,
+                                'price': price,
+                                'link': link})
+
+    
+    return figures
+
+def get_all_figures():
+    return(
+        scrape(LINK_URLS["green_rock_dc"], SHOP_URLS["greenrock"], SHOP["green_rock"]) +
+        scrape(LINK_URLS["green_rock_marvel"], SHOP_URLS["green_rock"], SHOP["green_rock"]) +
+        scrape(LINK_URLS["green_rock_transformers"], SHOP_URLS["green_rock"], SHOP["green_rock"])
+    )
