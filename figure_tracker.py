d925c998a4ffed99c66134d9ba5ed125d0e46180
Anfernee Alviar <anferneealviar99@hotmail.com>
2026-04-11 17:45:52 +0800

    added csv save and load functions

--- figure_tracker.py
+++ figure_tracker.py
@@ -1,5 +1,20 @@
 import csv
 
+def save_all_figures(rows):
+    
+    with open("figures.csv", "w") as file:
+        writer = csv.writer(file)
+        writer.writerows(rows)
+    
+    print("All figures saved!")
+    
+def load_all_figures():
+    with open("figures.csv", 'r') as file:
+        reader = csv.reader(file)
+        rows = list(reader)
+
+    return rows
+    
 def show_status_menu():
     print("1. Leaked")
     print("2. Announced")
@@ -8,145 +23,145 @@ def show_status_menu():
     print("5. Sold Out")
     print()
 
+def update_status():
+    pass
+
 def show_tier_menu():
     pass
 
 def update_figures():
-    with open("figures.csv", 'r') as file:
-        reader = csv.reader(file)
-        rows = list(reader)
+    rows = load_all_figures()
         
-        fig_rows = rows[1:]
+    fig_rows = rows[1:]
+    
+    count = 1
+    total = len(fig_rows)
+    
+    print("Which figure would you like to edit?")
+    while True:
+        for row in fig_rows:
+            print(f"{count}. {row[0]}")
+            count += 1
         
+        print("0. Go Back")
         count = 1
-        total = len(fig_rows)
+        option = input(f"Pick a number between 1 and {total}, or press 0 to go back: ")
         
-        print("Which figure would you like to edit?")
-        while True:
-            for row in fig_rows:
-                print(f"{count}. {row[0]}")
-                count += 1
+        if option.isdigit() is False:
+            print("Please pick a number.")
+            print()
+        
+        elif int(option) == 0:
+            break
+        
+        elif int(option) > total:
+            print("Please pick a valid option.")
+            print()
             
-            print("0. Go Back")
-            count = 1
-            option = input(f"Pick a number between 1 and {total}, or press 0 to go back: ")
-            
-            if option.isdigit() is False:
-                print("Please pick a number.")
-                print()
-            
-            elif int(option) == 0:
-                break
-            
-            elif int(option) > total:
-                print("Please pick a valid option.")
-                print()
+        else: 
+            figure = fig_rows[int(option)-1] 
+        
+            while True:
+                print(f"Updating {figure[0]}.")
                 
-            else: 
-                figure = fig_rows[int(option)-1] 
-            
-                while True:
-                    print(f"Updating {figure[0]}.")
+                print("What would you like to update on this figure?")
+                print("1. Name")
+                print("2. Release Date")
+                print("3. Status")
+                print("4. Tier")
+                print("5. Stores")
+                print("0. Go back")
+                
+                update = input("Please enter an option: ")
+                
+                if update.isdigit is False:
+                    print("Please enter a valid option.")
                     
-                    print("What would you like to update on this figure?")
-                    print("1. Name")
-                    print("2. Release Date")
-                    print("3. Status")
-                    print("4. Tier")
-                    print("5. Stores")
-                    print("0. Go back")
-                    
-                    update = input("Please enter an option: ")
-                    
-                    if update.isdigit is False:
-                        print("Please enter a valid option.")
-                        
-                    else:
-                        match(int(update)):
-                            case 1:
-                                pass
-                            case 2:
-                                pass
-                            case 3:
-                                print("== UPDATING STATUS == ")
-                                while True:
-                                    show_status_menu()
-                                    status = input("What status would you like to update this figure to? ")
-                                
-                                    if status.isdigit is False:
+                else:
+                    match(int(update)):
+                        case 1:
+                            pass
+                        case 2:
+                            pass
+                        case 3:
+                            print("== UPDATING STATUS == ")
+                            while True:
+                                show_status_menu()
+                                status = input("What status would you like to update this figure to? ")
+                            
+                                if status.isdigit is False:
+                                    print("Please enter a valid option.")
+                                else:
+                                    if int(status) > 5:
                                         print("Please enter a valid option.")
                                     else:
-                                        if int(status) > 5:
-                                            print("Please enter a valid option.")
-                                        else:
-                                            while True:
-                                                match(int(status)):
-                                                    case 1:
-                                                        rows[int(option)][2] = "Leaked"
-                                                        break
-                                                    case 2:
-                                                        rows[int(option)][2] = "Announced"
-                                                        break
-                                                    case 3:
-                                                        rows[int(option)][2] = "Preorder"
-                                                        with open('figures.csv', 'w', newline='') as file:
-                                                            writer = csv.writer(file)
-                                                            writer.writerows(rows)
-                                                        break
-                                                    case 4:
-                                                        rows[int(option)][2] = "Released"
-                                                        with open('figures.csv', 'w', newline='') as file:
-                                                            writer = csv.writer(file)
-                                                            writer.writerows(rows)
-                                                        break
-                                                    case 5: 
-                                                        rows[int(option) + 1][2] = "Sold Out"
-                                                        break
-                                                    case _:
-                                                        print("Please enter a valid option.")
-                                    
-                                            
-                                        break
-                            case 4: 
-                                pass
-                            case 5:
-                                pass
-                            case 0:
-                                break
-                            case _:
-                                print("Please enter a valid option.")
-                                print()
+                                        while True:
+                                            match(int(status)):
+                                                case 1:
+                                                    rows[int(option)][2] = "Leaked"
+                                                    break
+                                                case 2:
+                                                    rows[int(option)][2] = "Announced"
+                                                    break
+                                                case 3:
+                                                    rows[int(option)][2] = "Preorder"
+                                                    with open('figures.csv', 'w', newline='') as file:
+                                                        writer = csv.writer(file)
+                                                        writer.writerows(rows)
+                                                    break
+                                                case 4:
+                                                    rows[int(option)][2] = "Released"
+                                                    with open('figures.csv', 'w', newline='') as file:
+                                                        writer = csv.writer(file)
+                                                        writer.writerows(rows)
+                                                    break
+                                                case 5: 
+                                                    rows[int(option) + 1][2] = "Sold Out"
+                                                    break
+                                                case _:
+                                                    print("Please enter a valid option.")
+                                
+                                        
+                                    break
+                        case 4: 
+                            pass
+                        case 5:
+                            pass
+                        case 0:
+                            break
+                        case _:
+                            print("Please enter a valid option.")
+                            print()
                         
                     
 
 def show_figures():
-    with open("figures.csv", 'r') as file:
-        reader = csv.reader(file)
-        rows = list(reader)
+    
+    rows = load_all_figures()
         
-        fig_rows = rows[1:]
+    fig_rows = rows[1:]
+    
+    for row in fig_rows:
+        name = row[0]
+        print(f"Name: {name}")
         
-        for row in fig_rows:
-            name = row[0]
-            print(f"Name: {name}")
-            
-            release_date = row[1]
-            print(f"Release Date: {release_date}")
-            
-            status = row[2]
-            print(f"Status: {status}")
-            
-            tier = row[3]
-            print(f"Tier: {tier}")
-            
-            if row[4] != "":
-                website_list = row[4].split(";")
-                for website in website_list:
-                    store, url = website.split(":", 1)
-                    
-                    print(f"Store: {store.strip()} ({url.strip()})")
+        release_date = row[1]
+        print(f"Release Date: {release_date}")
+        
+        status = row[2]
+        print(f"Status: {status}")
+        
+        tier = row[3]
+        print(f"Tier: {tier}")
+        
+        if row[4] != "":
+            website_list = row[4].split(";")
+            for website in website_list:
+                store, url = website.split(":", 1)
                 
-            print()
+                print(f"Store: {store.strip()} ({url.strip()})")
+            
+        print()
                 
             
 def add_figure():
@@ -208,9 +223,7 @@ def add_figure():
         
     rows.append([figure_details["name"], figure_details["release_date"], figure_details["status"], figure_details["tier"], websites_str])
     
-    with open("figures.csv", 'w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerows(rows)
+    save_all_figures(rows)
         
     print(f"Added: {figure_details['name']}")
     
